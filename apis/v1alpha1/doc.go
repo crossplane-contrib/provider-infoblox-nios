44d6c0db24980f5db3507165fb0e6b407cbabf01
Steven Borrelli <steve@borrelli.org>
2024-06-04 17:58:51 +0200

    migrate resources to crossplane.io group
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- apis/v1alpha1/doc.go
+++ apis/v1alpha1/doc.go
@@ -4,6 +4,6 @@ Copyright 2021 Upbound Inc.
 
 // Package v1alpha1 contains the core resources of the infoblox-nios jet provider.
 // +kubebuilder:object:generate=true
-// +groupName=infoblox-nios.upbound.io
+// +groupName=infoblox-nios.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
